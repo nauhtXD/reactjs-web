020d39dc8fde972d7d69ed07e82feeb00806a8d9
ptbtcute99 <52491473+ptbtcute99@users.noreply.github.com>
2020-12-22 12:34:18 +0700

    Updates

--- sw.js
+++ sw.js
@@ -79,7 +79,7 @@ var __wpo = {
   },
   "strategy": "changed",
   "responseStrategy": "cache-first",
-  "version": "12/22/2020, 12:31:04 PM",
+  "version": "12/22/2020, 12:34:16 PM",
   "name": "webpack-offline",
   "pluginVersion": "5.0.6",
   "relativePaths": false
